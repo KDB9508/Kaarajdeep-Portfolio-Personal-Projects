a083130440f9be30e804391d9dfff47250970751
Kaarajdeep <k.s.bindra2@newcastle.ac.uk>
2026-02-02 10:19:49 +0000

    The user can now choose to either make a custom list or have a list be made automatically with random elements

--- AlgorithmVisualiser/LinearSearch.py
+++ AlgorithmVisualiser/LinearSearch.py
@@ -1,7 +1,38 @@
-def linearSearch(arr,n):
+print("1: Manually add elements in the array")
+print("2: Choose array size and have all elements randomly generated(range - 0 to 100)")
+option = int(input("Enter 1 or 2 to choose one of the options from above: "))
+
+arr = []
+num = ""
+
+while option not in [1,2]:
+    print("Please choose one of the options from above")
+    option = int(input("Enter 1 or 2 to choose one of the options from above: "))
+
+if option == 1:
+    while True:
+        element = input("Enter an element in the array or press ENTER to stop: ")
+        if element == "":
+            break
+        arr.append(int(element))
+
+    print(arr)
+    num = int(input("Enter the number that needs to be found in the array: "))
+
+elif option == 2:
+    import random
+    size = int(input("Enter the size of the array: "))
+    for s in range(size):
+        element = random.randint(0, 100)
+        arr.append(element)
+
+    print(arr)
+    num = int(input("Enter the number that needs to be found in the array: "))
+
+def linearSearch(arr, num):
     for i in range(len(arr)):
-        if arr[i] == n:
-            return i
+        if arr[i] == num:
+            return print(i)
     return None
 
-linearSearch([2,3,5,7,1,8,0,10], 7)
+linearSearch(arr, num)
